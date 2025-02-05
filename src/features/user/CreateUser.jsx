9019dd1858063a482627dbc9fbfd3cf1d2b7794d
Abanoub Nasry <bonypop9@gmail.com>
2025-02-05 14:09:12 +0200

    edit start button

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -31,7 +31,7 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button type="primary" to={"fast-react-pizza/menu"}>
+          <Button type="primary" to={"/fast-react-pizza/menu"}>
             Start ordering
           </Button>
         </div>
