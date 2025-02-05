9d2f6d7c2366ca2ef42beb1ad9c6bc4e5db4a3cd
Abanoub Nasry <bonypop9@gmail.com>
2025-02-05 14:06:11 +0200

    link a path to the start ordering button

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -31,7 +31,9 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button type="primary">Start ordering</Button>
+          <Button type="primary" to={"fast-react-pizza/menu"}>
+            Start ordering
+          </Button>
         </div>
       )}
     </form>
