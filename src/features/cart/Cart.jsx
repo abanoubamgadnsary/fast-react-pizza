d3fe77e59da57d7bba659a394611314e5484ca53
Abanoub Nasry <bonypop9@gmail.com>
2025-02-06 11:08:18 +0200

    q

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -13,7 +13,7 @@ function Cart() {
   if (!cart.length) return <EmptyCart />;
   return (
     <div className="px-4 py-3">
-      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+      <LinkButton to="menu/">&larr; Back to menu</LinkButton>
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
@@ -23,7 +23,7 @@ function Cart() {
         ))}
       </ul>
       <div className="mt-6 space-x-2">
-        <Button to="/order/new" type="primary">
+        <Button to="order/new/" type="primary">
           Order pizzas
         </Button>
 
